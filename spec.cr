3376957ea07de4a6f55040e36e806d7726f0a887
rogerdpack <rogerpack2005@gmail.com>
2021-06-10 20:40:34 -0600

    add spec

--- spec.cr
+++ spec.cr
@@ -1,3 +1,13 @@
 require "spec"
+require "./replacepdf.cr"
+
+describe "works" do
+
+  it "should replace" do
+    out = transmogrify("[(O)-16(ther i)-20(nformati)-11(on )]TJ", "Other", "zzz")
+    out.should eq("[zzz information ]TJ")
+  end
+
+end
 
 
