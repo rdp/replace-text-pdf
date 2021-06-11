ccc66abcc7e5d873cf52d9f59209cb4874ca18bd
rogerdpack <rogerpack2005@gmail.com>
2021-06-10 20:58:25 -0600

    moh

--- replacepdf.cr
+++ replacepdf.cr
@@ -1,13 +1,24 @@
 def transmogrify(input, replace_this, with_this)
 
 
-# [(O)-16(ther i)-20(nformati)-11(on )]TJ
+  # [(O)-16(ther i)-20(nformati)-11(on )]TJ
+  # assume for now TJ boxes don't span lines?
+  # assume for now we don't support more than single TJ box ...
 
-# assume for now TJ boxes don't span lines?
-# assume for now we don't support more than single TJ box ...
-
-  input = input.gsub(/\[.*?\]TJ/) { | moi |
+  input = input.gsub(/\[(.*?)\]TJ/) { | moi |
     puts moi
+    puts removeGlyph(moi)
   }
   input
 end
+
+def removeGlyph(input) # (O)-16(ther i)2(b) => Other ib
+  # assume balanced for now...XXX handle it not having glyph at all, I think that's legal...
+  # XXX escaped parens...
+  without_interior_parents = input.gsub(/\)-?\d*\(/, "") 
+  # remove beginning and ending now...
+  without_interior_parents[1..-2]
+end
+
+
+
--- spec.cr
+++ spec.cr
@@ -3,6 +3,10 @@ require "./replacepdf.cr"
 
 describe "works" do
 
+  it "should remove old glyph spacing" do
+    removeGlyph("(O)-16(ther i)2(b)").should eq "Other ib"
+  end
+
   it "should replace" do
     out = transmogrify("[(O)-16(ther i)-20(nformati)-11(on )]TJ", "Other", "zzz")
     out.should eq("[zzz information ]TJ")
