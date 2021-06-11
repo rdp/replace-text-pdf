c383db022807ec104e5abc44fe5985cf767df663
rogerdpack <rogerpack2005@gmail.com>
2021-06-10 21:35:26 -0600

    works

--- methods.cr
+++ methods.cr
@@ -5,24 +5,25 @@ def transmogrify(input, replace_this, with_this)
   # assume for now TJ boxes don't span lines?
   # assume for now we don't support more than single TJ box ...
 
-  output = input.gsub(/\[(.*?)\]TJ/) { | moi |
+  output = input.gsub(/\[.*?\]TJ/) { | moi |
     puts moi
     cleaned = removeGlyph(moi)
+    puts "cleaned=" + cleaned
     if cleaned.includes?(replace_this)
-      cleaned.sub(replace_this, with_this)
+      "[" + cleaned.sub(replace_this, with_this) + "]TJ"
     else
-      moi
+      moi # original
     end
   }
   output
 end
 
-def removeGlyph(input) # (O)-16(ther i)2(b) => Other ib
-  # assume balanced for now...XXX handle it not having glyph at all, I think that's legal...
-  # XXX escaped parens...
-  without_interior_parents = input.gsub(/\)-?\d*\(/, "") 
-  # remove beginning and ending now...
-  without_interior_parents[1..-2]
+def removeGlyph(input) # [(O)-16(ther i)2(b)]TJ => Other ib
+  # assume balanced for now...XXX handle it not having glyph at all, I think that's allowable...
+  # XXX handle escaped parens...
+  without_interior_parens = input.gsub(/\)-?\d*\(/, "") 
+  # remove beginning and ending junk now...
+  without_interior_parens[2..-5]
 end
 
 
--- spec.cr
+++ spec.cr
@@ -4,12 +4,17 @@ require "./methods.cr"
 describe "works" do
 
   it "should remove old glyph spacing" do
-    removeGlyph("(O)-16(ther i)2(b)").should eq "Other ib"
+    removeGlyph("[(O)-16(ther i)2(b)]TJ").should eq "Other ib"
   end
 
   it "should replace" do
     out = transmogrify("[(O)-16(ther i)-20(nformati)-11(on )]TJ", "Other", "zzz")
     out.should eq("[zzz information ]TJ")
+  end
+
+  it "should replace with caps" do
+    out = transmogrify("[(O)-16(ther i)-20(nformati)-11(on )]TJ", "information", "Zzz")
+    out.should eq("[Other Zzz ]TJ")
   end
 
   it "should retain other lines" do
@@ -18,9 +23,8 @@ describe "works" do
   end
 
   it "should replace more than one instance" do
-    out = transmogrify("stuff\n[(O)-16(ther i)-20(nformati)-11(on )]TJ\n[(OO)-16(ther i)]TJ\nstuff2\n[(O)-16(ther i)-20(nformati)-11(on )]TJ", "Other", "zzz")
-    out.should eq("stuff\n[zzz information ]TJ\n[(OO)-16(ther i)]TJ\nstuff2\n[zzz information ]TJi\nstuff2")
-
+    out = transmogrify("stuff\n[(O)-16(ther i)-20(nformati)-11(on )]TJ\n[(ZZ)-16(ther i)]TJ\nstuff2\n[(O)-16(ther i)-20(nformati)-11(on )]TJ\nstuff3", "Other", "zzz")
+    out.should eq("stuff\n[zzz information ]TJ\n[(ZZ)-16(ther i)]TJ\nstuff2\n[zzz information ]TJ\nstuff3")
   end
 
 end
