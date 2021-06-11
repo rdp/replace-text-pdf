89716bf9e51313002d8f5e812be17c4ca3d43e2d
rogerdpack <rogerpack2005@gmail.com>
2021-06-10 20:23:45 -0600

    moi

--- main.cr
+++ main.cr
@@ -1,22 +1,12 @@
-if ARGV.size != 4 || ARGV.includes?("-h") || ARGV.includes?("--help")
+if ARGV.size != 4 || ARGV.includes?("-h") 
   puts "syntax: input_filename desired_old_text replace_with_this_text output_filename"
   exit 1
 end
 
+require "replacepdf.cr"
 
 input = File.read(ARGV[0])
 desired = ARGV[1]
 replace_with = ARGV[2]
-output = File.open(ARGV[3], "w")
-
-# [(O)-16(ther i)-20(nformati)-11(on )]TJ
-
-# assume for now TJ boxes don't span lines?
-# assume for now we don't support more than single TJ box ...
-
-input = input.gsub(/\[.*?\]TJ/) { | moi |
-
-  puts moi
-}
-
-
+output = transmogrify(input, desired, replace_with)
+File.write(ARGV[3], output)
--- replacepdf.cr
+++ replacepdf.cr
@@ -1,5 +1,13 @@
 def transmogifry(input, replace_this, with_this)
 
 
+# [(O)-16(ther i)-20(nformati)-11(on )]TJ
 
+# assume for now TJ boxes don't span lines?
+# assume for now we don't support more than single TJ box ...
+
+  input = input.gsub(/\[.*?\]TJ/) { | moi |
+    puts moi
+  }
+  input
 end
