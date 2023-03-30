fa94625b84a11d1f7934f06d6d0c1164d1c1e0ec
rogerdpack <rogerpack2005@gmail.com>
2023-03-29 22:11:48 -0600

    moh info if scripted

--- replaceinpdf.cr
+++ replaceinpdf.cr
@@ -16,7 +16,7 @@ replace_with = ARGV[2]
 
 if ARGV.size == 5
   regex_for_matching_lines = ARGV[4]
-  STDERR.puts "using regex #{regex_for_matching_lines}"
+  STDERR.puts "only replacing lines matching regex #{regex_for_matching_lines}"
 else
   regex_for_matching_lines = ".*"
 end
@@ -29,7 +29,7 @@ else
   File.write(ARGV[3], output)
 end
 
-STDERR.puts "wrote num_modified=#{count} #{ARGV[0]} -> #{ARGV[3]}"
+STDERR.puts "wrote num_modified=#{count} replacing '#{desired}' with '#{replace_with}' files: #{ARGV[0]} -> #{ARGV[3]}"
 if count == 0
-  STDERR.puts "no changes looking for '#{desired}', is pdf compressed perhaps? If so please decompress first."
+  STDERR.puts "  No changes made, is pdf compressed perhaps? If so please decompress first."
 end
