b93b036124939a6398ec34bf8e05f6a23595b9cf
rogerdpack <rogerpack2005@gmail.com>
2023-03-29 20:42:11 -0600

    wire in

--- replaceinpdf.cr
+++ replaceinpdf.cr
@@ -1,6 +1,7 @@
-if ARGV.size != 4 || ARGV.includes?("-h") 
+if ARGV.size < 4 || ARGV.includes?("-h") 
   puts "syntax: input_filename desired_old_text replace_with_this_text output_filename"
   puts "  filenames can be - for stdin/stdout"
+  puts "  can have an option another parameter of a regular expression 'only make changes on lines matching this regex' like a..b a.*b etc"
   exit 1
 end
 
@@ -15,7 +16,13 @@ end
 desired = ARGV[1]
 replace_with = ARGV[2]
 
-output, count = transmogrify(input, desired, replace_with)
+if ARGV.size == 5
+  regex_for_matching_lines = ARGV[4]
+else
+  regex_for_matching_lines = ".*"
+end
+
+output, count = transmogrify(input, desired, replace_with, regex_for_matching_lines)
 
 if ARGV[3] == "-"
   STDOUT.print output
